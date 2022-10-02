28ce3eb62c67b1ba6a7c4c0a4e1b43f082cb4a9d
Guru Sarvankar <gururefresh@gmail.com>
2022-10-02 12:03:18 +0300

    upload project

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div class="container">
+            
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-10 col-xl-8 text-center">
+                <h1 class="my-5">Assignment Project</h1>
+                </div>
+                </div>
+        </div>
+    )
+}
---
+++ src/components/index.js
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from "react";
+
+
+const Index = () => {
+    
+    const[users, setUsers] = useState([]);
+
+    const getUsers =async() => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const FinalData = await response.json();
+        setUsers(FinalData)
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+  
+
+    return (
+      
+        <div className="container">
+           <div className="row">
+
+            {
+                users.map((curElem) => {
+                    return (
+                        <div className="col-lg-3 col-md-4 col-sm-6 text-center mb-4">
+                            <div class="card testimonial-card"  key={curElem.id}>
+                                <div class="card-up"></div>
+                                <div class="avatar mx-auto bg-white">
+                                    <div className="avatar-text border border-1 rounded-circle ">{curElem.name}</div>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">{curElem.name}</h5>
+                                    <p class="card-text">{curElem.username}</p>
+                                    <a href="#!" className="card-link mt-3 text-danger">{curElem.email}</a>
+                                    <div class="d-grid gap-2 mt-5">
+                                        <a href="#!" class="btn-lg btn-primary">See More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+            </div>
+        </div>              
+
+
+)
+}
+
+export default Index;
